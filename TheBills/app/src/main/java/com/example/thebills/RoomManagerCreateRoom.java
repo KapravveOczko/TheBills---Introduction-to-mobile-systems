5c3bb728521f025708596240c8c05671eec717f6
Kacper Czernik <Kacper-czernik@wp.pl>
2024-01-06 20:56:43 +0100

    project | hahahahah it works (after some improvements)

--- TheBills/app/src/main/java/com/example/thebills/RoomManagerCreateRoom.java
+++ TheBills/app/src/main/java/com/example/thebills/RoomManagerCreateRoom.java
@@ -71,6 +71,7 @@ public class RoomManagerCreateRoom extends AppCompatDialogFragment {
 
         if (!roomName.isEmpty()) {
             roomManager.createNewRoom(roomName);
+            roomManager.moveToRoomActivity(this.context);
         }
     }
 }
--- TheBills/app/src/main/java/com/example/thebills/RoomManagerJoinRoom.java
+++ TheBills/app/src/main/java/com/example/thebills/RoomManagerJoinRoom.java
@@ -68,5 +68,6 @@ public class RoomManagerJoinRoom extends AppCompatDialogFragment {
 
         if (!desiredRoomKey.isEmpty()) {
             roomManager.joinRoom(desiredRoomKey);
+            roomManager.moveToRoomActivity(this.context);
     }}
 }
